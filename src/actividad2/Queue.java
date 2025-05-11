7d964dec7f1e5dae8ec664f39ec88396c8c00665
maldest <andre.eguia01@gmail.com>
2025-05-10 21:54:09 -0500

    Creacion de la clase QueueLink

--- src/actividad2/Queue.java
+++ src/actividad2/Queue.java
@@ -4,5 +4,13 @@ import actividad2.ExceptionIsEmpty;
 
 //creacion de la clase generica para la cola
 public interface Queue<E>{
-    a
+    void enqueue(E x); //metodo para agregar elementos a la col
+    E dequeue() throws ExceptionIsEmpty; //elimina y devuelve el primer elemento
+    E front() throws ExceptionIsEmpty;
+    E back() throws ExceptionIsEmpty;
+    boolean isEmpty();
+    
+
+
+
 }
