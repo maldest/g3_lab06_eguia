db87efd5a5373c5ee25a43c529ed3be65bd8cf06
maldest <andre.eguia01@gmail.com>
2025-05-10 21:57:00 -0500

    Creacion del constructor

--- src/actividad2/QueueLink.java
+++ src/actividad2/QueueLink.java
@@ -1,5 +1,28 @@
 package actividad2;
 
 public class QueueLink {
+    private Node<E> first; // primer nodo (frente de la cola)
+    private Node<E> last;  // último nodo (final de la cola)
+
+    // Constructor, inicializa la cola vacía
+    public QueueLink() {
+        this.first = null;
+        this.last = null;
+    }
     
+    public void enqueue(E x){
+        Node<E> aux = new Node<E>(x);
+        if (this.isEmpty()){
+            this.first = aux;
+        } else{
+            this.last.setNext(aux);
+        }
+        this.last = aux;
+    }
+
+    
+
+
+
+
 }
